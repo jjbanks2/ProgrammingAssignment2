6afe4633ec3ded32aa4fdc35abeac9401c7b019f
jjbanks2 <jenbanks2@gmail.com>
2023-10-21 10:48:14 -0700

    Clean-up and final comments

--- cachematrix.R
+++ cachematrix.R
@@ -4,11 +4,12 @@
 # or calculate the inverse if not found in cache.  Code is based on
 # makeVector() and cachemean() from rdpeng/ProgrammingAssignment2.
 
-# Creates a special "matrix" object that includes a list
-# of setter and getter functions for the value of the matrix
-# and its inverse
 
+# Creates a special "matrix" object with setter and getter
+# object functions for the value of the matrix and its inverse, and
+# returns a list of the object functions
 makeCacheMatrix <- function(x = matrix()) {
+
     inv <- NULL
     set <- function(y) {
         x <<- y
@@ -23,22 +24,32 @@ makeCacheMatrix <- function(x = matrix()) {
     getinverse <- function() {
         inv
     }
-    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+    list(set = set, get = get, setinverse = setinverse, 
+         getinverse = getinverse)
 }
 
 
 # Takes a makeCacheMatrix object as input, and calculates the matrix
 # inverse, or returns it from cache if previously stored 
-
 cacheSolve <- function(x, ...) {
     
+    # checks if inverse is in cache
     inv <- x$getinverse()
+
+    # if inverse is in cache, return the value 
     if(!is.null(inv)) {
-        message("getting cached data")
         return (inv)
     }
+    
+    # if inverse is not in cache, retrieve the matrix
     d <- x$get()
+
+    # calculate inverse    
     inv <- solve(d)
+    
+    # add inverse to cache
     x$setinverse(inv)
+
+    # return calculated inverse
     inv
 }
